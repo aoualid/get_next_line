9f5676ee75870cc7a735e359e18f558de1709dd2
aroualid <aroualid@student.42.fr>
2023-12-29 12:09:27 +0100

    1

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,17 @@
 /*   By: aroualid <aroualid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 13:36:19 by aroualid          #+#    #+#             */
-/*   Updated: 2023/12/08 13:36:20 by aroualid         ###   ########.fr       */
+/*   Updated: 2023/12/29 12:09:22 by aroualid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+#endif
